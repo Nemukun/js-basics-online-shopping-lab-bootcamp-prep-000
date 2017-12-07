3da8379baab0cba91783b08f06ba50a89a1d61bd
Azumi Callanan <azucm@hotmail.com>
2017-12-07 23:37:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,27 +18,16 @@ function addToCart(item) {
 
 function viewCart() {
   if(cart.length===0){
-    console.log "Your shopping cart is empty.";
+    console.log ("Your shopping cart is empty.");
   }else{
-  var items=[];
     for(var i=0;i<cart.length;i++){
-    for(var item in cart[i]){
-      items.push
+      for(var item in cart[i]){
+        cart.push(`${item} has been added to your cart.`);
     }
     }
-}
 }
 }
 
-var items = [];
- +    for (var i = 0; i < cart.length; i++) {
- +      for (var item in cart[i]) {
- +        items.push(item + " at $" + cart[i][item])
- +      }
- +    }
- +    console.log("In your cart, you have " + items.join(", ") + ".");
- +  }
- +}
 
 function total() {
   // write your code here
